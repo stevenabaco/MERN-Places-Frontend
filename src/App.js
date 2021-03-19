e5e82df1dedc203a53d394d453fb62d14e0d4613
= <stevenabaco@gmail.com>
2021-03-19 16:12:45 -0500

    Add project folder structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import GoalList from './components/GoalList/GoalList';
-import NewGoal from './components/NewGoal/NewGoal';
-import './App.css';
-
-const App = () => {
-	const [courseGoals, setCourseGoals] = useState([
-		{ id: 'cg1', text: 'Finish the course' },
-		{ id: 'cg2', text: 'Learn all about course main topic' },
-		{ id: 'cg3', text: 'Help other students in the Course Q&A' },
-	]);
-
-	const addNewGoalHandler = newGoal => {
-    // setCourseGoals(courseGoals.concat(newGoal));
-    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
-	};
-
-	return (
-		<div className='course-goals'>
-			<h2>Course Goals</h2>
-			<NewGoal onAddGoal={addNewGoalHandler} />
-			<GoalList goals={courseGoals} />
-		</div>
-	);
-};
+function App() {
+	return <h1>Let's start!</h1>;
+}
 
 export default App;
