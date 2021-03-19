0729a868c0018941044b390b6ed09bee34234027
= <stevenabaco@gmail.com>
2021-03-19 17:20:30 -0500

    Add Users Route using Router and Switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
-function App() {
-	return <h1>Let's start!</h1>;
+import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace'
+
+const App = () => {
+	return <Router>
+		<Switch>
+			<Route exact path="/">
+				<Users />
+			</Route>
+			<Route exact path="/places/new">
+				<NewPlace />
+			</Route>
+			<Redirect to="/" />
+		</Switch>
+	</Router>
+
 }
 
 export default App;
