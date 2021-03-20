c3dc6369ccc437d68b278992228becfa141cfe68
= <stevenabaco@gmail.com>
2021-03-20 13:32:46 -0500

    Add basic navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+
 import './MainNavigation.css';
 
 const MainNavigation = props => {
   return (
+    <React.Fragment>
+    <SideDrawer>
+      <nav className='main-navigation__drawer-nav'>
+        <NavLinks/>
+      </nav>
+    </SideDrawer>
 		<MainHeader>
 			<button className="main-navigation__menu-btn">
 				<span />
@@ -15,8 +24,9 @@ const MainNavigation = props => {
       <h1 className="main-navigation__title">
         <Link to="/">React Places</Link>
       </h1>
-      <nav>...</nav>
-		</MainHeader>
+      <nav className='main-navigation__header-nav'><NavLinks/></nav>
+      </MainHeader>
+      </React.Fragment>
 	);
 };
 
