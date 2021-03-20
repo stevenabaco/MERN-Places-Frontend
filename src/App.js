c4daa6ccb9e62cfe29b61bbc7c1fba6b90ce5241
= <stevenabaco@gmail.com>
2021-03-20 11:48:14 -0500

    Add Main Header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-import Users from './user/pages/Users'
-import NewPlace from './places/pages/NewPlace'
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-	return <Router>
-		<Switch>
-			<Route exact path="/">
-				<Users />
-			</Route>
-			<Route exact path="/places/new">
-				<NewPlace />
-			</Route>
-			<Redirect to="/" />
-		</Switch>
-	</Router>
-
-}
+	return (
+		<Router>
+			<MainNavigation />
+			<main>
+				<Switch>
+					<Route exact path='/'>
+						<Users />
+					</Route>
+					<Route exact path='/places/new'>
+						<NewPlace />
+					</Route>
+					<Redirect to='/' />
+				</Switch>
+			</main>
+		</Router>
+	);
+};
 
 export default App;
