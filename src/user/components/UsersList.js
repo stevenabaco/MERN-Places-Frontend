8538d545d81938d22ff8fac6a75ff47bc9cec475
= <stevenabaco@gmail.com>
2021-03-20 11:28:04 -0500

    Add Navigation Component and update folder structure

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import './UsersList.css';
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
+
 
 const UsersList = props => {
 	if (props.items.length === 0) {
