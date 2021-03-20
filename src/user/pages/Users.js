403d4628560a346ea7e0d27c267c0b5c4aca4af5
= <stevenabaco@gmail.com>
2021-03-19 21:02:58 -0500

    Add UserList Page

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-	return <h2>Users Works!</h2>;
+	const USERS = [
+		{
+			id: 'u1',
+			name: 'Steven Abaco',
+			image:
+				'https://images.unsplash.com/photo-1615747476205-a76551ac8636?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+			places: 3,
+		},
+	];
+	return <UsersList items={USERS} />;
 };
 
 export default Users;
