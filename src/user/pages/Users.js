f06ce289509beecbc762f5ff8eb79ec1016b082f
= <stevenabaco@gmail.com>
2021-03-20 09:28:47 -0500

    Add Avatar with Card and styling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,8 +7,7 @@ const Users = () => {
 		{
 			id: 'u1',
 			name: 'Steven Abaco',
-			image:
-				'https://images.unsplash.com/photo-1615747476205-a76551ac8636?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+			image: 'https://www.stevenabaco.dev/static/media/me.4043ab91.jpg',
 			places: 3,
 		},
 	];
