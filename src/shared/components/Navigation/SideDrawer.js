9ede032682a6f66dbd02f601200e39a792ef3091
= <stevenabaco@gmail.com>
2021-03-20 14:34:46 -0500

    Fix SIdeDrawer to close on click inside drawer

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -6,8 +6,16 @@ import './SideDrawer.css';
 
 const SideDrawer = props => {
 	const content = (
-		<CSSTransition in={props.show} timeout={200} classNames='slide-in-left' mountOnEnter unmountOnExit>
-			<aside className='side-drawer'>{props.children}</aside>
+		<CSSTransition
+			in={props.show}
+			timeout={200}
+			classNames='slide-in-left'
+			mountOnEnter
+			unmountOnExit
+		>
+			<aside className='side-drawer' onClick={props.onClick}>
+				{props.children}
+			</aside>
 		</CSSTransition>
 	);
 
