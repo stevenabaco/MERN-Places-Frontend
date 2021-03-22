69c6bad738e2415409f74278f0079bf962d489f5
= <stevenabaco@gmail.com>
2021-03-21 19:12:34 -0500

    Add custom buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -13,6 +14,9 @@ const App = () => {
 				<Switch>
 					<Route exact path='/'>
 						<Users />
+					</Route>
+					<Route exact path='/:userId/places'>
+						<UserPlaces/>
 					</Route>
 					<Route exact path='/places/new'>
 						<NewPlace />
