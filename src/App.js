2745c1cc5245e271d45acad8853c3efe32d157fa
= <stevenabaco@gmail.com>
2021-03-23 14:46:09 -0500

    Add Custom Form Input

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
+console.log(`${process.env.REACT_APP_GOOGLE_API_KEY}`);
+
 const App = () => {
 	return (
 		<Router>
