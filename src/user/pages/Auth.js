dfb7c8f74bec7fa601d23be2d8963901c964900d
Steven Abaco <stevenabaco@gmail.com>
2021-07-05 17:03:20 -0500

    Add proper error handling for Front End

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -4,6 +4,8 @@ import Input from '../../shared/components/FormElements/Input/Input';
 import Button from '../../shared/components/FormElements/Button/Button';
 import Card from '../../shared/components/UIElements/Card/Card';
 import { useForm } from '../../shared/hooks/form-hook';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner/LoadingSpinner';
 import {
 	VALIDATOR_EMAIL,
 	VALIDATOR_MINLENGTH,
@@ -15,6 +17,9 @@ import { AuthContext } from '../../shared/context/auth-context';
 const Auth = () => {
 	const auth = useContext(AuthContext);
 	const [isLoginMode, setIsLoginMode] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
+
 	const [formState, inputHandler, setFormData] = useForm(
 		{
 			email: {
@@ -58,72 +63,87 @@ const Auth = () => {
 		event.preventDefault();
 
 		if (isLoginMode) {
-
 		} else {
 			try {
+				setIsLoading(true);
 				const response = await fetch('http://localhost:5000/api/users/signup', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				name: formState.inputs.name.value,
-				email: formState.inputs.email.value,
-				password: formState.inputs.password.value,
-			}),
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						name: formState.inputs.name.value,
+						email: formState.inputs.email.value,
+						password: formState.inputs.password.value,
+					}),
 				});
+
 				const responseData = await response.json();
+
+				if (!response.ok) {
+					throw new Error(responseData.message);
+				}
+
 				console.log(responseData);
+				setIsLoading(false);
+				auth.login();
 			} catch (err) {
-				console.log(err) // Temp error handling
+				console.log(err); // Temp error handling
+				setIsLoading(false);
+				setError(err.message || 'Something went wrong, please try again.');
 			}
 		}
-		
+	};
 
-		auth.login();
+	const errorHandler = () => {
+		setError(null);
 	};
 	return (
-		<Card className='authentication'>
-			<h2>Login Required</h2>
-			<hr />
-			<form onSubmit={authSubmitHandler}>
-				{!isLoginMode && (
+		<>
+			<ErrorModal error={error} onClear={errorHandler} />
+			<Card className='authentication'>
+				{isLoading && <LoadingSpinner asOverlay />}
+				<h2>Login Required</h2>
+				<hr />
+				<form onSubmit={authSubmitHandler}>
+					{!isLoginMode && (
+						<Input
+							element='input'
+							id='name'
+							type='text'
+							label='Your Name'
+							validators={[VALIDATOR_REQUIRE()]}
+							onInput={inputHandler}
+							errorText='Please enter a name'
+						/>
+					)}
 					<Input
+						id='email'
 						element='input'
-						id='name'
-						type='text'
-						label='Your Name'
-						validators={[VALIDATOR_REQUIRE()]}
+						type='email'
+						label='Email'
+						validators={[VALIDATOR_EMAIL()]}
+						errorText='Please enter a valid Email address'
 						onInput={inputHandler}
-						errorText='Please enter a name'
 					/>
-				)}
-				<Input
-					id='email'
-					element='input'
-					type='email'
-					label='Email'
-					validators={[VALIDATOR_EMAIL()]}
-					errorText='Please enter a valid Email address'
-					onInput={inputHandler}
-				/>
-				<Input
-					id='password'
-					element='input'
-					type='password'
-					label='Password'
-					validators={[VALIDATOR_MINLENGTH(5)]}
-					errorText='Please enter a valid password, at least 5 characters'
-					onInput={inputHandler}
-				/>
-				<Button type='submit' disabled={!formState.isValid}>
-					{isLoginMode ? 'LOGIN' : 'SIGNUP'}
+					<Input
+						id='password'
+						element='input'
+						type='password'
+						label='Password'
+						validators={[VALIDATOR_MINLENGTH(5)]}
+						errorText='Please enter a valid password, at least 5 characters'
+						onInput={inputHandler}
+					/>
+					<Button type='submit' disabled={!formState.isValid}>
+						{isLoginMode ? 'LOGIN' : 'SIGNUP'}
+					</Button>
+				</form>
+				<Button inverse onClick={switchModeHandler}>
+					SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
 				</Button>
-			</form>
-			<Button inverse onClick={switchModeHandler}>
-				SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
-			</Button>
-		</Card>
+			</Card>
+		</>
 	);
 };
 
